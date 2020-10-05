3761f94d363217440e8ba8269abe9694b0d6d0fd
Wendy Grant <wendykgrant.tx@gmail.com>
2020-10-05 09:29:55 -0500

    Updated html & js - not pulling from open weather

--- script.js
+++ script.js
@@ -1,122 +1,175 @@
-// declare variables
+Skip to content
+Search or jump to…
 
-let city = "";
-let url = "";
-let apiKey = "";
-let currentURL = "";
-let forecastURL = "";
-let searchDiv = document.querySelector("search_history_container");
-let cityList = []
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@wkgrant78 
+acdollard
+/
+weather-dashboard
+1
+1
+1
+Code
+Issues
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+weather-dashboard/script.js /
+@acdollard
+acdollard moved click listener inside the render buttons function
+Latest commit b07be1b on Dec 3, 2019
+ History
+ 1 contributor
+145 lines (118 sloc)  4.65 KB
+  
 
-// check local storage on page query
-function searchHistory() {
-    let stored_city = JSON.parse(localStorage.getItem("cityList"));
+let city=""; 
+let url="";
+let APIkey="";
+let queryurl ="";
+let currenturl = "";
+let citiesDiv = document.getElementById("searched_cities_container");
+//start with empty array
+let cities = []; 
+init(); 
+listClicker(); 
+searchClicker(); 
 
-    // load search history
-    if (stored_city !== null) {
-        cityList = stored_city
-    }
+//run function to pull saved cities from local storage and fill array with it
+function init(){
+    let saved_cities = JSON.parse(localStorage.getItem("cities"));
 
-    renderButtons();
+    if (saved_cities !== null){
+        cities = saved_cities
+    }   
+    
+    renderButtons(); 
 }
 
-// assign search history to localstorage
-function storeCitySearch() {
-    localStorage.setItem("cityList", JSON.stringify(cityList));
+//sets localstorage item to cities array 
+function storeCities(){
+    localStorage.setItem("cities", JSON.stringify(cities)); 
 }
 
-// render click event btn in searchDiv
-function renderButton() {
-    searchDiv.innerHTML = "";
-    if (cityList == null) {
+
+//render buttons for each element in cities array as a search history for user
+function renderButtons(){
+    citiesDiv.innerHTML = ""; 
+    if(cities == null){
         return;
     }
-
-    let new_cities = [... new Set(cityList)];
-    for (let i = 0; i < new_cities.length; i++) {
-        let cityName = new_cities[i];
+    let unique_cities = [...new Set(cities)];
+    for(let i=0; i < unique_cities.length; i++){
+        let cityName = unique_cities[i]; 
 
         let buttonEl = document.createElement("button");
-        buttonEl.textContent = cityName;
-        buttonEl.setAttribute("class", "searchBtn");
+        buttonEl.textContent = cityName; 
+        buttonEl.setAttribute("class", "listbtn"); 
 
-        searchDiv.appendChild(buttonEl);
+        citiesDiv.appendChild(buttonEl);
         listClicker();
+      }
     }
-}
-
-// on click test
-// function listClicker() {
-//     $(".listBtn").on("click", function (event) {
-//         console.log("anybody home?")
-//         event.preventDefault();
-//         console.log("hello?");
-//         city = $(this).text().trim();
-//         callAPI();
-//     })
-// }
-
-// on click for city search event
-function searchEvent(){
-    $("#searchBtn").on("click", function (event){
-        event.preventDefault();
-        city = $(this).prev().val().trim()
-
-        // push city searched to cityList (max 5)
-        cityList.push(city);
-        if (cityList.length > 5){
-            cityList.shift()
-        }
-        if (city == ""){
-            return;
-        }
-        callAPIs();
-        storeCitySearch();
-        renderButton
-    })
+//on click function for search history buttons
+function listClicker(){
+$(".listbtn").on("click", function(event){
+    console.log("anybody home?")
+    event.preventDefault();
+    console.log("hello?");
+    city = $(this).text().trim();
+    APIcalls(); 
+})
 }
 
 
 
+//on click function for main search bar
+function searchClicker() {
+$("#searchbtn").on("click", function(event){
+    event.preventDefault();
+    city = $(this).prev().val().trim()
+    
+    //push the city user entered into cities array 
+    cities.push(city);
+    //make sure cities array.length is never more than 8 
+    if(cities.length > 8){
+        cities.shift()
+    }
+    //return from function early if form is blank
+    if (city == ""){
+        return; 
+    }
+    APIcalls();
+    storeCities(); 
+    renderButtons();
+})
+}
+
+//runs 2 API calls, one for current weather data and one for five-day forecast, then populates text areas
+function APIcalls(){
+    
+    url = "https://api.openweathermap.org/data/2.5/forecast?q=";    
+    currenturl = "https://api.openweathermap.org/data/2.5/weather?q=";
+    APIkey = "&appid=a46d7ace8fe9b30fe73ee26488f40d18";
+    queryurl = url + city + APIkey;
+    current_weather_url = currenturl + city + APIkey; 
+    
+    $("#name_of_city").text("Today's Weather in " + city);
+    $.ajax({
+        url: queryurl,
+        method: "GET",
+        
+    }).then(function(response){
+        let day_number = 0; 
+        
+        //iterate through the 40 weather data sets
+        for(let i=0; i< response.list.length; i++){
+            
+            //split function to isolate the time from the time/data aspect of weather data, and only select weather reports for 3pm
+            if(response.list[i].dt_txt.split(" ")[1] == "15:00:00")
+            {
+                //if time of report is 3pm, populate text areas accordingly
+                let day = response.list[i].dt_txt.split("-")[2].split(" ")[0];
+                let month = response.list[i].dt_txt.split("-")[1];
+                let year = response.list[i].dt_txt.split("-")[0];
+                $("#" + day_number + "date").text(month + "/" + day + "/" + year); 
+                let temp = Math.round(((response.list[i].main.temp - 273.15) *9/5+32));
+                $("#" + day_number + "five_day_temp").text("Temp: " + temp + String.fromCharCode(176)+"F");
+                $("#" + day_number + "five_day_humidity").text("Humidity: " + response.list[i].main.humidity);
+                $("#" + day_number + "five_day_icon").attr("src", "http://openweathermap.org/img/w/" + response.list[i].weather[0].icon + ".png");
+                console.log(response.list[i].dt_txt.split("-"));
+                console.log(day_number);
+                console.log(response.list[i].main.temp);
+                day_number++; 
+                        }   
+        }
+    });
+
+
+    //function to display data in main div 
+     $.ajax({
+         url:current_weather_url,
+         method: "GET", 
+     }).then(function(current_data){
+         console.log(current_data);
+         let temp = Math.round(((current_data.main.temp - 273.15) * 9/5 + 32))
+         console.log("The temperature in " + city + " is: " + temp);
+         $("#today_temp").text("Temperature: " + temp + String.fromCharCode(176)+"F");
+         $("#today_humidity").text("Humidity: " + current_data.main.humidity);
+         $("#today_wind_speed").text("Wind Speed: " + current_data.wind.speed);
+         $("#today_icon_div").attr({"src": "http://openweathermap.org/img/w/" + current_data.weather[0].icon + ".png",
+          "height": "100px", "width":"100px"});
+     })
+    
+
+}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function callAPI() {
-
-//     // Open Weather API
-//     currentURL = "https://api.openweathermap.org/data/2.5/weather?q=";
-
-//     forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q="; 
-
-//     apiKey = "&appid=a46d7ace8fe9b30fe73ee26488f40d18";
-
-
-//     // query URLs
-//     queryURL1 = currentURL + city + apiKey;
-
-//     // call API for current weather in "city"
-//     // 
-
-//     $.ajax({
-//         url: queryURL1,
-//         method: "GET",
-//     }).then(function(current_data) {
-//         console.log(current_data);
-
-//     })
